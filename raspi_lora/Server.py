6de96f1634ec810e71ed144071fd7edde27bb808
shonkap <32246364+shonkap@users.noreply.github.com>
2024-12-13 13:47:23 -0800

    Update Server.py

--- raspi_lora/Server.py
+++ raspi_lora/Server.py
@@ -13,7 +13,7 @@ def on_recv(message):
 
 try:
 	print(ModemConfig.Bw125Cr45Sf128)
-	newlora = LoRa(1, 5, 2, modem_config=ModemConfig.Bw125Cr45Sf128, tx_power=14, acks=True, receive_all=False)
+	newlora = LoRa(1, 5, 2, modem_config=ModemConfig.Bw125Cr45Sf128, tx_power=14, acks=True, receive_all=True)
 
 	newlora.on_recv = on_recv
 except Exception as e:
@@ -26,15 +26,12 @@ message = "Hello there!"
 status = newlora.send_to_wait(message, 5, retries=2) #255 is all
 if status is False:
 		print("No acknowledge from recipient")
-newlora.set_mode_rx()
 
 print(	"########################################\n",
 	"########################################\n","quit to exit\n",
      	"########################################");
 i = 0
 while True:
-		if self._interruptReceived:
-			newlora._handle_interrupt(1)
 		if select.select([sys.stdin],[],[], 0.1)[0]:
 			lineData = sys.stdin.readline().strip()
 			if lineData.lower() == "quit":
@@ -42,6 +39,5 @@ while True:
 			newlora.send_to_wait(lineData,5,retries=2)
 			if status is False:
 					print("No acknowledge from recipient")
-			newlora.set_mode_rx()
 newlora.close()
 
